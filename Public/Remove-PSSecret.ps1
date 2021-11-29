76b1deb52f27b43c3556d1b4e201fa660d07a8d6
Harish <harish.karthic@gmail.com>
2021-11-29 15:03:33 +0000

    Remove a secret value using it's Id

--- Public/Remove-PSSecret.ps1
+++ Public/Remove-PSSecret.ps1
@@ -1,7 +1,32 @@
 function Remove-PSSecret {
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High', DefaultParameterSetName = "Id")]
     param (
-        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(
+            Mandatory = $true, 
+            Position = 0, 
+            ValueFromPipeline = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = "Id")]
+        [Parameter(
+            Mandatory = $true, 
+            Position = 0, 
+            ValueFromPipeline = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = "Both")]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter([IdCompleter])]
+        [int] $Id,
+
+        [Parameter(
+            Mandatory = $true, 
+            Position = 0, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = "Name")]
+        [Parameter(
+            Mandatory = $true, 
+            Position = 1, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = "Both")]
         [ArgumentCompleter([NameCompleter])]
         [ValidateNotNullOrEmpty()]
         [string] $Name,
@@ -11,11 +36,24 @@ function Remove-PSSecret {
     
     process {
         if (_isValidConnection (_getConnectionObject)) {
-            if (!(_isNameExists $Name)) { Write-Warning "Couldn't find the value for given Name '$Name'; Pass the correct value and try again." }
 
-            else {
-                if ($Force -or $PSCmdlet.ShouldProcess($Name, "Remove-Secret")) {
-                    Invoke-SqliteQuery -DataSource (_getDbPath) -Query "DELETE FROM _ WHERE Name = '$Name'"
+            if ($Force -or $PSCmdlet.ShouldProcess($Name, "Remove-Secret")) {
+                if ($PSCmdlet.ParameterSetName -eq "Id") {
+                    Invoke-SqliteQuery -DataSource (_getDbPath) -Query "DELETE FROM _ WHERE Id = '$Id'"
+                }
+
+                if ($PSCmdlet.ParameterSetName -eq "Name") {
+                    $res = Get-PSSecret -Name $Name
+
+                    if ($res.Count -eq 1) {
+                        Invoke-SqliteQuery -DataSource (_getDbPath) -Query "DELETE FROM _ WHERE Name = '$Name'"
+                    } else {
+                        Write-Warning "More than (1) values found for given Name '$Name'; Pass Id to remove the respective value from the vault."
+                    }
+                }
+
+                if ($PSCmdlet.ParameterSetName -eq "Both") {
+                    Invoke-SqliteQuery -DataSource (_getDbPath) -Query "DELETE FROM _ WHERE Name = '$Name' AND Id = '$Id'"
                 }
             }
         } else { _connectionWarning }
